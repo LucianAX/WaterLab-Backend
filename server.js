e11f7967c7d0f6f2d090f39765b554772e8517ad
unknown <lucian.alexcoseru@yahoo.com>
2021-11-02 23:36:48 +0100

    Node, npm, Express, Sqlite3 have been bootstrapped into the project

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
+
+const apiRouter = require('./api/api.js');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(errorHandler());
+
+
+const PORT = process.env.PORT || 4000; //optionally set port to process.env.PORT value if it exists, for testing purposes
+
+apiRouter.use('/api', apiRouter);
+
+app.use(express.static('public')); //serves index.html on PORT if accessed in browser 
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`);
+});
+
+module.exports = app; //for use in the test file
