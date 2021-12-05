2b66c8ad5478f6d4405fc5757da81d9be3bf9aa2
unknown <lucian.alexcoseru@yahoo.com>
2021-12-05 21:10:29 +0100

    Project separated into 3 different apps: backend, client-frontend, client-embedded. This remains the backend.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './render_measurements.js',
+  entry: './server.js',
   output: {
     filename: '_bundle.js',
     path: path.resolve(__dirname)    
