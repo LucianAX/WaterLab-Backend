0455e008ec57035be37025f898b1493a0a7ee141
unknown <lucian.alexcoseru@yahoo.com>
2022-03-25 22:55:55 +0100

    US 5.2 Finished Bend functionality. Created new PUT route for stationary unit router

--- api/stationaryUnit.js
+++ api/stationaryUnit.js
@@ -25,22 +25,36 @@ stationaryUnitRouter.get('/:id', (req, res, next) => {
     );
 });
 
-// stationaryUnitRouter.get('/', (req, res, next) => {    
-//     db.all(`
-//         SELECT *
-//         FROM StationaryUnit;
-//         `, (err, stationaryUnits) => {
-//             if (err) {
-//                 next(err);
-//             }
-//             if (!stationaryUnits) {
-//                 res.status(404).send('No stationary unit found!');                
-//             } else  {
-//                 res.status(200).send({ stationaryUnit: stationaryUnits });
-//                 console.log(stationaryUnits);
-//             }
-//         }
-//     );
-// });
+stationaryUnitRouter.put('/:id', (req, res, next) => {
+    const ID = req.params.id;
+    const newMeasurementInterval = req.body.measurementInterval;
+
+    db.run(`
+        UPDATE StationaryUnit
+        SET interval_execute_measurement = $newInterval
+        WHERE id = $ID;
+        `,
+        {
+            $ID: ID,
+            $newInterval: newMeasurementInterval
+        }, 
+        function(err) {
+            if (err) {
+                return next(err);
+            }
+            db.get(`
+                SELECT interval_execute_measurement
+                FROM StationaryUnit
+                WHERE id = $ID;
+                `, { $ID: ID },
+                (err, updatedInterval) => {
+                    err
+                        ? next(err)
+                        : res.status(200).send({ updatedInterval: updatedInterval });
+                }
+            );
+        }
+    );
+});
 
 module.exports = stationaryUnitRouter;
