c21a26fdc99bf950ddcd9977b2db90e7c83e2b2f
unknown <lucian.alexcoseru@yahoo.com>
2022-03-26 23:13:01 +0100

    US 5.3 modified DB schema for StationaryUnit so it has new column is_timer_active

--- scripts/utils/database_creation.js
+++ scripts/utils/database_creation.js
@@ -17,6 +17,7 @@ db.serialize(() => {
             id INTEGER PRIMARY KEY NOT NULL,
             unit_name TEXT NOT NULL,
             interval_execute_measurement INTEGER NOT NULL,
+            is_timer_active TEXT DEFAULT 'false',
             limit_ph_minimum REAL NOT NULL,
             limit_ph_maximum REAL NOT NULL,
             limit_temp_minimum REAL NOT NULL,
--- scripts/utils/seed_stationary_unit.js
+++ scripts/utils/seed_stationary_unit.js
@@ -2,44 +2,44 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./waterlab_database.sqlite');
 
 // One Stationary Unit with ID 1 has already been created !!!
-db.run(`
-    INSERT INTO StationaryUnit
-        (unit_name, interval_execute_measurement,
-         limit_ph_minimum, limit_ph_maximum,
-         limit_temp_minimum, limit_temp_maximum,
-         limit_ec_minimum, limit_ec_maximum)
-    VALUES
-        ($unitName, $intervalExecMeas,
-         $limitPhMin, $limitPhMax,
-         $limitTempMin, $limitTempMax,
-         $limitEcMin, $limitEcMax);
-    `,
-    {
-        $unitName: 'WaterLab PoC',
-        $intervalExecMeas: 12,
-        $limitPhMin: 6.0,
-        $limitPhMax: 7.0,
-        $limitTempMin: 12.0,
-        $limitTempMax: 20.0,
-        $limitEcMin: 0.005,
-        $limitEcMax: 0.051
-    },
-    function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        db.get(`
-            SELECT *
-            FROM StationaryUnit
-            WHERE id = ${this.lastID};
-            `, (err, newUnit) => {
-                err
-                    ? console.log(err)
-                    : console.log(newUnit);
-            }
-        );
-    }
-);
+// db.run(`
+//     INSERT INTO StationaryUnit
+//         (unit_name, interval_execute_measurement,
+//          limit_ph_minimum, limit_ph_maximum,
+//          limit_temp_minimum, limit_temp_maximum,
+//          limit_ec_minimum, limit_ec_maximum)
+//     VALUES
+//         ($unitName, $intervalExecMeas,
+//          $limitPhMin, $limitPhMax,
+//          $limitTempMin, $limitTempMax,
+//          $limitEcMin, $limitEcMax);
+//     `,
+//     {
+//         $unitName: 'WaterLab PoC',
+//         $intervalExecMeas: 12,
+//         $limitPhMin: 6.0,
+//         $limitPhMax: 7.0,
+//         $limitTempMin: 12.0,
+//         $limitTempMax: 20.0,
+//         $limitEcMin: 0.005,
+//         $limitEcMax: 0.051
+//     },
+//     function(err) {
+//         if (err) {
+//             return console.log(err);
+//         }
+//         db.get(`
+//             SELECT *
+//             FROM StationaryUnit
+//             WHERE id = ${this.lastID};
+//             `, (err, newUnit) => {
+//                 err
+//                     ? console.log(err)
+//                     : console.log(newUnit);
+//             }
+//         );
+//     }
+// );
 
 // db.run(`
 //     ALTER TABLE StationaryUnit
@@ -51,4 +51,25 @@ db.run(`
 // // RENAME COLUMN warning_temp_maximum TO limit_temp_maximum;
 // // RENAME COLUMN warning_ec_minimum TO limit_ec_minimum;
 // // RENAME COLUMN warning_ec_maximum TO limit_ec_maximum;
+// );
+
+
+// db.run(`
+//     ALTER TABLE StationaryUnit
+//     ADD COLUMN is_timer_active TEXT DEFAULT 'false';
+//     `
+// );
+
+// db.run(`
+//     UPDATE StationaryUnit
+//     SET is_timer_active = $status
+//     WHERE id = $ID;
+//     `, 
+//     {
+//         $ID: 1,
+//         $status: 'false'
+//     },
+//     (err) => {
+//         if (err) console.log(err);
+//     }
 // );
